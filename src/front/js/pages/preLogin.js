91d7c5591a252eacd9d337b5d96b9cf374116f9a
Jose Pindado <joseangelpindado@gmail.com>
2025-01-10 20:17:36 +0000

    Jose_Pindado_10/01/2025

--- src/front/js/pages/preLogin.js
+++ src/front/js/pages/preLogin.js
@@ -11,27 +11,40 @@ export const PreLogin = () => {
     const navigate = useNavigate()
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Pagina para dar de alta el restaurante o hacer login</h1>
-			<Link to="/restRegister">
-                <span className="text  m-5">
-                    Go to restaurant register
-                </span>
-            </Link>
-			<Link to="/secLogin">
-                <span className="text">
-                    Go to secLogin 
-                </span>
-            </Link>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+		<div>
+			<div className="text-center mt-5">
+				<h1>Pagina para dar de alta el restaurante o hacer login</h1>
+				<Link to="/restRegister">
+					<span className="text  m-5">
+						Go to restaurant register
+					</span>
+				</Link>
+				<Link to="/secLogin">
+					<span className="text">
+						Go to secLogin 
+					</span>
+				</Link>
+				<div className="alert alert-info">
+					{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+				</div>
+
+				<div className="containerpreLogin">
+     				<div className="row mt-5 justify-content-center">
+       					<div className="col-12 text-center text-white mt-5">
+          					<h1>Da de alta tu restaurante</h1>
+         					<button className="btn btn-secondary mt-3">Click aquí</button>
+        				</div>
+      				</div>
+      				<div className="row mt-5 justify-content-center">
+        				<div className="col-6 text-center text-white mb-5">
+         					<h1>Inicia sesión</h1>
+          					<input type="text" className="form-control mt-3" placeholder="Usuario" />
+          					<input type="password" className="form-control mt-3" placeholder="Contraseña" />
+          					<button className="btn btn-secondary mt-3">Click aquí</button>
+        				</div>
+     				</div>
+   				</div>
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation otra cosa
-				</a>
-			</p>
 		</div>
 	);
 };
--- src/front/js/pages/secLogin.js
+++ src/front/js/pages/secLogin.js
@@ -8,26 +8,44 @@ import { Link, useNavigate } from "react-router-dom";
 export const SecLogin = () => {
 
     return (
-        <div className="text-center mt-5">
-            <h2>
-                esta donde se da entrada al administrador o a la tablet de la mesa
-            </h2>
-            <Link to="/">
-                <span className="text  m-5">
-                    Return to preLogin
-                </span>
-            </Link>
-            <Link to="/menusView">
-                <span className="text  m-5">
-                    Go to menusView
-                </span>
-            </Link>
-            <Link to="/tableMenu">
-                <span className="text">
-                    Go to tableMenu
-                </span>
-            </Link>
-
+        <div>
+            <div className="text-center mt-5">
+                <h2>
+                    Esta donde se da entrada al administrador o a la tablet de la mesa
+                </h2>
+                <Link to="/">
+                    <span className="text  m-5">
+                        Return to preLogin
+                    </span>
+                </Link>
+                <Link to="/menusView">
+                    <span className="text  m-5">
+                        Go to menusView
+                    </span>
+                </Link>
+                <Link to="/tableMenu">
+                    <span className="text">
+                        Go to tableMenu
+                    </span>
+                </Link>
+            </div>
+            <div className="containerpreLogin">
+     			<div className="row mt-5 justify-content-center">
+       				<div className="col-6 text-center text-white mt-5">
+          				<h1>Administración</h1>
+                        <input type="text" className="form-control mt-3" placeholder="Código admin" />
+         				<button className="btn btn-secondary mt-3">Click aquí</button>
+        			</div>
+      			</div>
+      			<div className="row mt-5 justify-content-center">
+        			<div className="col-6 text-center text-white mb-5">
+         				<h1>Identificador mesa</h1>
+          				<input type="text" className="form-control mt-3" placeholder="Código mesa" />
+          				<input type="password" className="form-control mt-3" placeholder="Seleccionar menú" />
+          				<button className="btn btn-secondary mt-3">Click aquí</button>
+        			</div>
+     			</div>
+   			</div>
         </div>
 
     );
