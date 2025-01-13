a2b22b3af707646bcf423ba50413ac2991d94c20
Gabriella Bevilacqua <bevi.gaby@gmail.com>
2025-01-13 17:31:51 +0000

    dropdown funcionando

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -23,13 +23,42 @@ export const Navbar = () => {
                         <img src={imagenLogo} alt="Logo" style={{ height: "60px" }} />
                         <div className="ml-auto">
                             <div className="dropdown">
-                                <button className="buttonNavbar dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <button className="buttonNavbar dropdown-toggle"
+                                    type="button"
+                                    id="dropdownMenuButton"
+                                    data-bs-toggle="dropdown"
+                                    aria-haspopup="true"
+                                    aria-expanded="false">
                                     Mesa x Cliente x Pedidos x
                                 </button>
-                                <div className="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-                                    <a className="dropdown-item" href="#">Action</a>
-                                    <a className="dropdown-item" href="#">Another action</a>
-                                    <a className="dropdown-item" href="#">Something else here</a>
+                                <div className="dropdown-menu dropdown-menu-end w-auto" aria-labelledby="dropdownMenuButton">
+                                    <a className="dropdown-item" href="#">Pedido ---------------Cantidad-----Estatus------Precio</a>
+                                    <p className="dropdown-item mb-0">
+                                        ----------------------------------------------------<br />
+                                        Bebida: Coca-cola---------02-------Servido--------5$<br />
+                                        Bebida: Cuba libre--------01-------Servido--------6$<br />
+                                        Entrante: Ensalada--------01-------Servido--------12$<br />
+                                        Entrante: Patatas---------02-------Servido--------25$
+                                    </p>
+                                    <a className="dropdown-item" href="#">Pedido 01</a>
+                                    <p className="dropdown-item mb-0">
+                                        ----------------------------------------------------<br />
+                                        Principal: Hamburguesa----02------En cocción-----40$<br />
+                                        Principal: Entrecot--------01-----En cocción--------20$
+                                    </p>
+                                    <a className="dropdown-item" href="#">Pedido 02</a>
+                                    <p className="dropdown-item mb-0">
+                                        ----------------------------------------------------<br />
+                                        Postre: Helado--------01--------En espera--------6$<br />
+                                        Postre: Copa---------02---------En espera--------12$
+                                    </p>
+                                    <a className="dropdown-item space-between" href="#">
+                                        Pedido 03
+                                        <button className="buttonDropdown" type="button">Enviar pedido a cocina</button>
+                                    </a>
+                                    <p className="dropdown-item mb-0">
+                                        Subtotal -----------------------------------------180$
+                                    </p>
                                 </div>
                             </div>
                         </div>
