77c918b1f9d152f2596a52eb154ede8a948ecb64
Gabriella Bevilacqua <bevi.gaby@gmail.com>
2025-01-17 15:43:05 +0000

    bolita arreglada en logines

--- src/front/js/pages/preLogin.js
+++ src/front/js/pages/preLogin.js
@@ -10,17 +10,17 @@ import tableClick01 from "../../img/tableClick01.png";
 
 export const PreLogin = () => {
 	const { store, actions } = useContext(Context);
-	const [username, setUsername] = useState(""); 
-	const [password, setPassword] = useState(""); 
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
 	const navigate = useNavigate()
 
-	const handleLogin = async () => { 
-		const success = await actions.loginUser(username, password); 
-		if (success) { 
+	const handleLogin = async () => {
+		const success = await actions.loginUser(username, password);
+		if (success) {
 			navigate("/secLogin"); // Redirigir a la página de inicio de sesión 
-		} else { 
-			alert("Login failed. Please check your credentials and try again."); 
-		} 
+		} else {
+			alert("Login failed. Please check your credentials and try again.");
+		}
 	};
 
 	return (
@@ -34,37 +34,48 @@ export const PreLogin = () => {
 				</Link>
 			</div>
 			<div className="text-center mt-2">
-			<img src={tableClick01} alt="Logoapp" style={{ height: "100px" }} />
+				<img src={tableClick01} alt="Logoapp" style={{ height: "100px" }} />
 			</div>
 
-			<div className="dot1"></div>
-		
-			<div className ="containerLogin">
-			
-				<div className="row mt-3 justify-content-center">
-					<div className="col-6 text-center text-white mt-5">
-						<h1>Da de alta tu restaurante</h1>
-						<Link to="/restRegister">
-							<button className="button4 mt-3">Click aquí</button>
-						</Link>
+
+			<div className="container outer mt-5">
+
+				<div className="dot1"></div>
+				<div className="card card2">
+
+					<div className="containerLogin">
+
+						<div className="row mt-4 justify-content-center">
+							<div className="col-6 text-center text-white mt-5">
+								<h1>Da de alta tu restaurante</h1>
+								<Link to="/restRegister">
+									<button className="button4 mt-3">Click aquí</button>
+								</Link>
+							</div>
+						</div>
+						<div className="row mt-5 justify-content-center">
+							<div className="col-6 text-center text-white">
+								<h1>Inicia sesión</h1>
+								<input type="text" className="form-control mt-3" placeholder="Usuario" />
+								<input type="password" className="form-control mt-3" placeholder="Contraseña" />
+								<Link to="/secLogin">
+									<button className="button4 mt-3">Click aquí</button>
+								</Link>
+							</div>
+							<div className="text-center mt-5 mb-3">
+								<Link to="/">
+									<span className="span text-white text-center">Recuperar contraseña</span>
+								</Link>
+							</div>
+						</div>
 					</div>
+
+					<div className="line topl"></div>
+					<div className="line leftl"></div>
+					<div className="line bottoml"></div>
+					<div className="line rightl"></div>
 				</div>
-				<div className="row mt-5 justify-content-center">
-					<div className="col-6 text-center text-white">
-						<h1>Inicia sesión</h1>
-						<input type="text" className="form-control mt-3" placeholder="Usuario" />
-						<input type="password" className="form-control mt-3" placeholder="Contraseña" />
-						<Link to="/secLogin">
-							<button className="button4 mt-3">Click aquí</button>
-						</Link>
-					</div>
-					<div className="text-center mt-3 mb-5">
-						<Link to="/secLogin">
-							<span className="span text-white text-center">Recuperar contraseña</span>
-						</Link>
-					</div>
-				</div>
-			</div>			
+			</div>
 		</div>
 	);
 };
--- src/front/js/pages/secLogin.js
+++ src/front/js/pages/secLogin.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
+import "../../styles/preLogin.css"
 import { Link, useNavigate } from "react-router-dom";
 import tableClick01 from "../../img/tableClick01.png";
 
@@ -25,30 +26,44 @@ export const SecLogin = () => {
                 </Link>
             </div>
             <div className="text-center mt-2">
-                        <img src={tableClick01} alt="Logoapp" style={{ height: "100px" }} />
+                <img src={tableClick01} alt="Logoapp" style={{ height: "100px" }} />
+            </div>
+
+
+            <div className="container outer mt-5">
+
+                <div className="dot1"></div>
+                <div className="card card2">
+
+                    <div className="containerpreLogin">
+                        <div className="row mt-5 justify-content-center">
+                            <div className="col-6 text-center text-white mt-5">
+                                <h1>Administración</h1>
+                                <input type="text" className="form-control mt-3" placeholder="Código admin" />
+                                <Link to="/menusView">
+                                    <button className="button4 mt-3">Click aquí</button>
+                                </Link>
+                            </div>
                         </div>
-            <div className="containerpreLogin">
-                <div className="row mt-3 justify-content-center">
-                    <div className="col-6 text-center text-white mt-5">
-                        <h1>Administración</h1>
-                        <input type="text" className="form-control mt-3" placeholder="Código admin" />
-                        <Link to="/menusView">
-                            <button className="button4 mt-3">Click aquí</button>
-                        </Link>
+                        <div className="row mt-5 mb-5 justify-content-center">
+                            <div className="col-6 text-center text-white mb-5">
+                                <h1>Identificador mesa</h1>
+                                <input type="text" className="form-control mt-3" placeholder="Código mesa" />
+                                <input type="password" className="form-control mt-3" placeholder="Seleccionar menú" />
+                                <Link to="/tableMenu">
+                                    <button className="button4 mt-3">Click aquí</button>
+                                </Link>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="row mt-5 justify-content-center">
-                    <div className="col-6 text-center text-white mb-5">
-                        <h1>Identificador mesa</h1>
-                        <input type="text" className="form-control mt-3" placeholder="Código mesa" />
-                        <input type="password" className="form-control mt-3" placeholder="Seleccionar menú" />
-                        <Link to="/tableMenu">
-                            <button className="button4 mt-3">Click aquí</button>
-                        </Link>
-                    </div>
+
+                    <div className="line topl"></div>
+                    <div className="line leftl"></div>
+                    <div className="line bottoml"></div>
+                    <div className="line rightl"></div>
+
                 </div>
             </div>
         </div>
-
     );
 };
