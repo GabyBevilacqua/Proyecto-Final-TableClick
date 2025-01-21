86e94b286e43f07802c79ebb29f3b547afebd651
Fernando Santana <fernandosbcn@gmail.com>
2025-01-21 00:36:16 +0000

    Button Dropdown navbar

--- src/front/js/component/dropNavigation.js
+++ src/front/js/component/dropNavigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import "../../styles/dropNavigation.css";
 
 
 export const DropNavigation = () => {
@@ -7,37 +8,98 @@ export const DropNavigation = () => {
     const userId = user.id
 
     return (
-        <div className="dropdown">
-            <button className="button2 dropdown-toggle me-3" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <span>@</span>
-            </button>
-            <ul className="dropdown-menu">
-                <Link to="/">
-                    <li><button className="dropdown-item" type="button">Home</button></li>
-                </Link>
-                <Link to="/secLogin">
-                    <li><button className="dropdown-item" type="button">Adim</button></li>
-                </Link>
-                <Link to={`/editUser/${userId}`}>
-                <li><button className="dropdown-item" type="button">Edit user</button></li>
-                </Link>
-                <Link to="/menusView">
-                <li><button className="dropdown-item" type="button">Menus Creados</button></li>
-                </Link>
-                <Link to="/menuItems">
-                <li><button className="dropdown-item" type="button">Carta del dia</button></li>
-                </Link>
-                <Link to="/tableMenu">
-                <li><button className="dropdown-item" type="button">Tablet</button></li>
-                </Link>
-                <Link to="/diningView">
-                <li><button className="dropdown-item" type="button">Comedor</button></li>
-                </Link>
-                <Link to="/">
-                <li><button className="dropdown-item" type="button">Cerrar sesion</button></li>
-                </Link>
-            </ul>
-        </div>
+        <div className="menu">
+  <div className="item">
+    <a href="#" className="link">
+      <span> El Cangrejo </span>
+      <svg viewBox="0 0 360 360" xmlSpace="preserve">
+        <g id="SVGRepo_iconCarrier">
+          <path
+            id="XMLID_225_"
+            d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393 c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393 s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"
+          ></path>
+        </g>
+      </svg>
+    </a>
+    <div className="submenu">
+      <div className="submenu-item">
+      <Link to="/">
+        <a href="#" className="submenu-link"> Home </a>
+        </Link>
+      </div>
+      <div className="submenu-item">
+      <Link to="/">
+        <a href="#" className="submenu-link"> Admin </a>
+        </Link>
+      </div>
+      <div className="submenu-item">
+      <Link to="/">
+        <a href="#" className="submenu-link"> Menus Creados </a>
+        </Link>
+      </div>
+      <div className="submenu-item">
+      <Link to="/">
+        <a href="#" className="submenu-link"> Carta del dia </a>
+        </Link>
+      </div>
+      <div className="submenu-item">
+      <Link to="/">
+        <a href="#" className="submenu-link"> Tablet </a>
+        </Link>
+      </div>
+      <div className="submenu-item">
+      <Link to="/">
+        <a href="#" className="submenu-link"> Comedor </a>
+        </Link>
+      </div>
+      <div className="submenu-item">
+      <Link to="/">
+        <a href="#" className="submenu-link"> Edit user </a>
+        </Link>
+      </div>
+      <div className="submenu-item">
+      <Link to="/">
+        <a href="#" className="submenu-link"> Cerrar sesion </a>
+        </Link>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+       
     );
 }
 
+{/* <div classNameName="dropdown">
+<button classNameName="button2 dropdown-toggle me-3" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    <span>@</span>
+</button>
+
+<ul classNameName="dropdown-menu">
+    <Link to="/">
+        <li><button classNameName="dropdown-item" type="button">Home</button></li>
+    </Link>
+    <Link to="/secLogin">
+        <li><button classNameName="dropdown-item" type="button">Adim</button></li>
+    </Link>
+    <Link to={`/editUser/${userId}`}>
+    <li><button classNameName="dropdown-item" type="button">Edit user</button></li>
+    </Link>
+    <Link to="/menusView">
+    <li><button classNameName="dropdown-item" type="button">Menus Creados</button></li>
+    </Link>
+    <Link to="/menuItems">
+    <li><button classNameName="dropdown-item" type="button">Carta del dia</button></li>
+    </Link>
+    <Link to="/tableMenu">
+    <li><button classNameName="dropdown-item" type="button">Tablet</button></li>
+    </Link>
+    <Link to="/diningView">
+    <li><button classNameName="dropdown-item" type="button">Comedor</button></li>
+    </Link>
+    <Link to="/">
+    <li><button classNameName="dropdown-item" type="button">Cerrar sesion</button></li>
+    </Link>
+</ul>
+</div> */}
